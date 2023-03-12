95f8b7fbc3450970b2e0c2b521208ff4fdd6e296
wandakim <nwd1583@naver.com>
2023-03-13 02:19:51 +0900

    Module: export,require

--- 1-node/4-module/counter.js
+++ 1-node/4-module/counter.js
@@ -1,3 +1,6 @@
+// node 자체적으로 만든 exports 시스템
+// ES6+ 에서는 Javascript 자체적으로 모듈을 지원해 주고 있다.
+
 let count = 0;
 
 function increase() {
@@ -13,5 +16,10 @@ module.exports.increase = increase;
 console.log(module.exports === exports);
 // exports = {};
 // console.log(module.exports === exports);
-exports.increase = increase;
+exports.increase = increase; // 위의 것과 엄밀히 다르다.
+/* 
+exports에 다른 값을 할당하면 위험할 수 있다. 
+처음에는 module.exports === exports true 이지만 
+exports에 다른 값을 할당하면 달라지는 것을 볼 수 있다. 
+*/
 console.log(module);

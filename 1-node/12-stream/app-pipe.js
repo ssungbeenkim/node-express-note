2e0012383045cb9c1e3aa951bc341e9669f4652a
wandakim <nwd1583@naver.com>
2023-03-13 12:20:19 +0900

    Node: strem,pipe

--- 1-node/12-stream/app-pipe.js
+++ 1-node/12-stream/app-pipe.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const zlib = require('zlib');
+const zlib = require('zlib'); // node.js애서 제공하는 압축 모듈
 
 const readStream = fs.createReadStream('./file.txt');
 const zlibStream = zlib.createGzip();
@@ -9,7 +9,7 @@ piping.on('finish', () => {
   console.log('done!!');
 });
 
-const http = require('http');
+const http = require('http'); //  서버를 만들 때 유용하게 사용할 수 있음.
 const server = http.createServer((req, res) => {
   const stream = fs.createReadStream('./file.txt');
   stream.pipe(res);

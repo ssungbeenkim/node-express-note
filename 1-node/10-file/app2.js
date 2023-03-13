a23147901269991be50bef9afff30c809c8d8344
wandakim <nwd1583@naver.com>
2023-03-13 12:01:22 +0900

    Node: Buffer

--- 1-node/10-file/app2.js
+++ 1-node/10-file/app2.js
@@ -1,25 +1,25 @@
 const fs = require('fs').promises;
 
 // reading a file
-fs.readFile('./text.txt', 'utf8') //
+fs.readFile('./text.txt', 'utf8') //인코딩 해주지 않으면 버퍼 형태로 출력됨
   .then((data) => console.log(data))
   .catch(console.error);
 
-// writing a file
-fs.writeFile('./file.txt', 'Hello, Dream Coders! :) ') //
-  .catch(console.error);
+// writing a file 기존의 데이터가 있으면 덮어쓰기
+fs.writeFile('./file.txt', 'Hello, Dream Coders! :) ') // 아무것도 리턴되지 않지만
+  .catch(console.error); // Void 리턴이므로 에러 발생 대비하여캐치는 꼭 해주기
 
-fs.appendFile('./file.txt', 'Yo!, Dream Coders! :) ') //
+fs.appendFile('./file.txt', 'Yo!, Dream Coders! :) ') // 기존의 데이터 유지
   .catch(console.error);
 
 // copy
-fs.copyFile('./file.txt', './file2.txt') //
-  .catch(console.error);
+fs.copyFile('./file.txt', './file2.txt') // 파일에 아무런 내용이 없다. 비동기적으로 처리가 되기 때문.
+  .catch(console.error); // 위에서 쓴 다음 처리를 해 주고 싶다면 .then()에 콜백 형태로 전달해주자.
 
 // folder
 fs.mkdir('sub-folder') //
   .catch(console.error);
 
-fs.readdir('./') //
+fs.readdir('./') // 배열의 형태로 파일과 폴더의 이름이 리턴된다.
   .then(console.log)
   .catch(console.error);
--- 1-node/11-buffer/app.js
+++ 1-node/11-buffer/app.js
@@ -3,15 +3,18 @@
 const fs = require('fs');
 
 const buf = Buffer.from('Hi');
-console.log(buf);
+console.log(buf); // 유니코드 형태로 출력된다.
+// 별도로 인코딩을 지정하지 않으면 UTF-8 인코딩 방식을 쓰게 됨
+// * If provided, the {encoding} parameter identifies the character encoding.
+// * If not provided, {encoding} defaults to 'utf8'.
 console.log(buf.length);
-console.log(buf[0]);
+console.log(buf[0]); // 배열에 있는 형태로 접근하면 ASCII code 출력이 된다.
 console.log(buf[1]);
 console.log(buf.toString());
 
 // create
-const buf2 = Buffer.alloc(2);
-const buf3 = Buffer.allocUnsafe(2); // fast
+const buf2 = Buffer.alloc(2); // 공간을 확보하고 초기화시켜줌
+const buf3 = Buffer.allocUnsafe(2); // fast , 초기화는 시키지 않음
 buf2[0] = 72;
 buf2[1] = 105;
 buf2.copy(buf3);

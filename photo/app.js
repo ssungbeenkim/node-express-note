e49b94d62a1c0a8ebceee993c5c483668a25c365
wandakim <nwd1583@naver.com>
2023-03-15 14:14:17 +0900

    photo script making

--- photo/app.js
+++ photo/app.js
@@ -16,7 +16,37 @@ const duplicatedDir = path.join(workingDir, 'duplicated');
 !fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);
 !fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);
 
-fs.promises
-  .readdir(workingDir) //
-  .then((files) => console.log(files))
-  .catch(console.error);
+fs.promises.readdir(workingDir).then(processFiles).catch(console.error);
+// readdir로 배열의 형태로 파일 이름들을 받아온다.
+
+function processFiles(files) {
+  files.forEach((file) => {
+    if (isVideoFile(file)) {
+      move(workingDir);
+    } else if (isCapturedFile(file)) {
+      console.log('captured', file);
+    } else if (isDuplicatedFile(files, file)) {
+      console.log('duplicated', file);
+    }
+  });
+}
+
+function isVideoFile(file) {
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+function isCapturedFile(file) {
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+function isDuplicatedFile(files, file) {
+  // IMG_XXXX -> IMG_EXXXX
+  if (!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+    return false;
+  }
+  const editted = `IMG_E${file.split('_')[1]}`;
+  const found = files.find((v) => v.includes(editted));
+  return !!found;
+}

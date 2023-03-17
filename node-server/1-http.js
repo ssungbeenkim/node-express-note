6984628bb2d76b264d93cd385ae6e07cc4276ca6
wandakim <nwd1583@naver.com>
2023-03-17 15:17:10 +0900

    node-serever: json

--- node-server/1-http.js
+++ node-server/1-http.js
@@ -5,10 +5,11 @@ const server = http.createServer((req, res) => {
   console.log('incoming...');
   const url = req.url;
   res.setHeader('Content-Type', 'text/html'); //setHeader를 지정해줘서 html임을 알려준다.
+  // Content-Type 헤더는 데이터의 MIME 타입을 나타내는 필드이다.
   if (url === '/') {
     // 아무 경로도 없다면
     fs.createReadStream('./html/index.html').pipe(res); // ?res 연결해주는 부분이 낯설다
-  } else if (url === '/course') {
+  } else if (url === '/courses') {
     fs.createReadStream('./html/courses.html').pipe(res);
   } else {
     fs.createReadStream('./html/not-found.html').pipe(res);
--- node-server/3-json.js
+++ node-server/3-json.js
@@ -1,20 +1,38 @@
+// 안드로이드나 아이폰 등 브라우저가 아닌 다른 클라이언트에서 다른 목적으로 서버의 데이터를 가져오기 위해서는
+// json 이나 다른 형태의 데이터를 제공할 수 있는 서버를 만들어야 한다.
+// 사용자게에 고정된, ui적인 html 파일을 제공하는 것이 아니라 json의 형태로 데이터를 제공하는 서버를 만들어보자.
+
 const http = require('http');
-const fs = require('fs');
+const courses = [
+  // 실제로 사용한다면 데이터베이스에 저장해 두고 사용할 것
+  { name: 'HTML' },
+  { name: 'CSS' },
+  { name: 'JS' },
+  { name: 'Node' },
+  { name: 'Front-end' },
+];
 
 const server = http.createServer((req, res) => {
-  console.log('incoming...');
-  const url = req.url;
-  res.setHeader('Content-Type', 'text/html'); //setHeader를 지정해줘서 html임을 알려준다.
-  if (url === '/') {
-    // 아무 경로도 없다면
-    fs.createReadStream('./html/index.html').pipe(res); // ?res 연결해주는 부분이 낯설다
-  } else if (url === '/course') {
-    fs.createReadStream('./html/courses.html').pipe(res);
-  } else {
-    fs.createReadStream('./html/not-found.html').pipe(res);
+  const url = req.url; // what?
+  const method = req.method; //how?, action?
+  if (url === '/courses') {
+    if (method === 'GET') {
+      res.writeHead(200, { 'content-Type': 'application/json' });
+      res.end(JSON.stringify(courses));
+    } else if (method === 'POST') {
+      const body = [];
+      req.on('data', (chunk) => {
+        body.push(chunk);
+      });
+      req.on('end', () => {
+        const bodyStr = Buffer.concat(body).toString();
+        const course = JSON.parse(bodyStr);
+        courses.push(course);
+        res.writeHead(201);
+        res.end();
+      });
+    }
   }
-
-  // 서버에 있는 resource를 클라이언트에 보내줄 수 있다.
 });
 
 server.listen(8080);

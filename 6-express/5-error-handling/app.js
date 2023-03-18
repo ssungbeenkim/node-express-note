11550a01869b482d72301ba66e0ea845b5c1cb3c
wandakim <nwd1583@naver.com>
2023-03-18 12:25:24 +0900

    error-handling

--- 6-express/5-error-handling/app.js
+++ 6-express/5-error-handling/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import fs from 'fs';
 import fsAsync from 'fs/promises';
-
+// 서버는 많은 클라이언트의 요청을 처리하는 곳이므로 에러처리는 중요하다.
+// express에서의 동기적, 비동기적 에러 처리 방법.
 const app = express();
 
 app.use(express.json());
@@ -10,18 +11,18 @@ app.get('/file1', (req, res) => {
   // 1.
   // fs.readFile('/file1.txt', (err, data) => {
   //   if (err) {
-  //     res.sendStatus(404);
+  //     res.sendStatus(404); // good
   //   }
   // });
 
-  // 2.
+  // 2. 동기적인 경우
   try {
     const data = fs.readFileSync('/file1.txt');
   } catch (error) {
-    res.sendStatus(404);
+    res.sendStatus(404); // 데이터가 존재하지 않는다.
   }
 });
-
+// 3. promise 비동기적인 경우
 app.get('/file2', (req, res) => {
   fsAsync
     .readFile('/file2.txt') //
@@ -42,8 +43,9 @@ app.get('/file3', async (req, res) => {
 
 // Express 5 부터는 이렇게
 app.use((error, req, res, next) => {
+  // 마지막에 에러핸들링을 해 주는 것이 좋다.
   console.error(error);
   res.status(500).json({ message: 'Something went wrong' });
 });
 
-app.listen(8080);
+app.listen(8081);

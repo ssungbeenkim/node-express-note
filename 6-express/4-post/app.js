fd71ace148f65906035d17ee7eef9553f4d0f234
wandakim <nwd1583@naver.com>
2023-03-20 08:54:07 +0900

    express: useful internal middlewares

--- 6-express/4-post/app.js
+++ 6-express/4-post/app.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.use(express.json()); // 모든 request에 express에서 제공하는 json을 사용.
 // express에서 body의 내용을 읽기 위해서는, 제공되는 미들웨어를 사용한다.
-// 요청이 들어오는 body 부분을 자돌으로 parsing해서 우리에게 보여줌
+// 요청이 들어오는 body 부분을 자동으로 parsing해서 우리에게 보여줌
+// 강의에서는 postman 사용해서 POST request 보내 주었음
 
 app.post('/', (req, res, next) => {
   console.log(req.body); // 사용자가 무언가 만들기 원하기 때문에 body를 읽어와야 한다.
--- 6-express/8-useful-internal/app.js
+++ 6-express/8-useful-internal/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
 
 const app = express();
+// express 내부에서 제공하는 유용한 미들웨어들
 
-// express.json -> REST API, body parse
-// express.urlencoded -> HTML form
-// express.static
+// app.use(express.json()); //-> REST API에서 body parse
+
+// app.use(express.urlencoded({ extended: false }));
+/* ☞ HTML form에서 submit 하면 발생되는 request에서 전달되는 data를 자동으로 parse */
+
+// app.use(express.static('public'));
+/* /index.html 또는 /image.png로 요청이 오면 해당 폴더 자동처리 해줌
+->app.get('/index.html', ....) 이렇게 작성해 줄 필요가 없다. */
 
 app.use(express.json());
 app.post('/posts', (req, res) => {
@@ -13,15 +19,17 @@ app.post('/posts', (req, res) => {
 });
 
 const options = {
-  dotfiles: 'ignore',
+  dotfiles: 'ignore', // 숨겨진 파일 보이지 않토록
   etag: false,
   index: false,
-  maxAge: '1d',
+  maxAge: '1d', // 캐싱 얼마나 오래
   redirect: false,
   setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now());
+    res.set('x-timestamp', Date.now()); //해더에 추가
   },
 };
 
-app.use(express.static('public', options));
-app.listen(8080);
+app.use(express.static('public', options)); // 다양한 옵션을 줄 수가 있다.
+app.listen(8081);
+
+// 이전 버전에서는 지원하지 않았으나 커뮤니티에서 많이 사용되다 보니 자체적으로 지원해 주게 됨

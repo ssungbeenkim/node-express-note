70056bd0823104e20a92fc6b7ad9dd64992635d7
wandakim <nwd1583@naver.com>
2023-04-20 10:27:50 +0900

    Edit os module part

--- 1-node/6-os/app.js
+++ 1-node/6-os/app.js
@@ -1,12 +1,16 @@
+/* os module
+os 모듈은 운영체제의 정보를 제공하는 모듈이다.*/
 const os = require('os');
 
-console.log(os.EOL === '\n');
-console.log(os.EOL === '\r\n');
+/* 줄바꿈 문자열 
+Windows에서는 줄바꿈 문자열이 \r\n이고, MacOS나 Linux에서는 줄바꿈 문자열이 \n이다. */
+console.log(os.EOL === '\n'); // true
+console.log(os.EOL === '\r\n'); // false
 
-console.log(os.totalmem());
-console.log(os.freemem());
-console.log(os.type());
-console.log(os.userInfo());
-console.log(os.cpus());
-console.log(os.homedir());
-console.log(os.hostname());
+console.log(os.totalmem()); // 시스템의 총 메모리를 바이트 단위로 반환
+console.log(os.freemem()); // 시스템의 사용 가능한 메모리를 바이트 단위로 반환
+console.log(os.type()); // 운영체제의 이름을 반환
+console.log(os.userInfo()); // 사용자 정보를 담은 객체를 반환
+console.log(os.cpus()); // CPU의 정보를 담은 객체를 반환
+console.log(os.homedir()); // 홈 디렉터리의 경로를 문자열로 반환
+console.log(os.hostname()); // 컴퓨터의 이름을 문자열로 반환
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-#node.js studynote
+# node.js studynote
